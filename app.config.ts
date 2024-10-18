e3788626dd05fe131216694cc66954eacc0e0c38
Mark Francis <mark@moonstar.ai>
2024-10-18 17:53:34 +0100

    Fix bundling failure due to cloudflare:sockets insert

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from '@solidjs/start/config';
 export default defineConfig({
   server: {
     preset: 'cloudflare-pages',
+    rollupConfig: {
+      external: ['cloudflare:sockets'],
+    },
   },
 });
